a05e33597be8076c62e6ecd92c65d843d8cae8b8
Bill Eggert <weggert@princeton.edu>
2016-12-04 16:38:03 -0500

    finished importing number data

--- src/processing/data_import_first_try.py
+++ src/processing/data_import_first_try.py
@@ -11,47 +11,60 @@ so here's a single pass thru for data processing
 # adapted from "traditional" python script for reading MNISt data"
 
 import os, struct
-import numpy as np
+from numpy import append, array, int8, uint8, zeros
 from array import array as pyarray
-import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import pyplot
 # step 1: import mmnist data
 
 # goal: load mnist data into 3D numpy arrays
 def load_mnist(dataset, digit, path):
     
     
+    """
+    Returns a set of two-element tuples
+    first element: label
+    second element: numpy 2D array of pixel data
+    """    
     
     if dataset == "training":
         image_path = path + '/train-images.idx3-ubyte'
         label_path = path + '/train-labels.idx1-ubyte'
     elif dataset == "testing":
-        image_path = os.path.join(path, 't10k-images-idx3-ubyte')
-        label_path = os.path.join(path, 't10k-labels-idx1-ubyte')
-        
-    with open(label_path, 'rb') as file:
+        image_path = path + '/t10k-images-idx3-ubyte'
+        label_path = path + '/t10k-labels-idx1-ubyte'
+    
+    with open(label_path,'rb') as file:
         num, size = struct.unpack(">II", file.read(8))
-        # error checking
         label_data = pyarray("b",file.read())
-
-    with open(image_path,'r') as file:
-        num, size, rows, cols = struct.unpack(">IIII", file.read(16))    
-        image_data = pyarray("B",file.read())
-      
-    print(label_data[0])
-    ind = [i for i in range(size) if label_data(i) in digit]
+        
+    with open(image_path,'rb') as file:
+        num, size, rows, cols = struct.unpack(">IIII", file.read(16))
+        image_data = pyarray("B", file.read())
+        
+    ind = [i for i in range(size) if label_data[i] in digit]
     N = len(ind)
     
+    #preallocate
+    images = zeros((N,rows,cols), dtype=uint8)
+    labels = zeros((N,1), dtype=int8)
+    
+    
     for i in range(N):
-        images[i] = np.array(image_data[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ])
-        images[i].reshape((rows,cols))
         
+        images[i] = array(image_data[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ]).reshape((rows, cols))
         labels[i] = label_data[ind[i]]
         
     return images, labels
+        
 
 test_path = "C:/Users/billi/Desktop/GitHub Repo/APC_FinalProject/data/mnist/testing"
 train_path = "C:/Users/billi/Desktop/GitHub Repo/APC_FinalProject/data/mnist/training"
+testdigit = [5]
+images, labels = load_mnist(dataset = "training", digit = testdigit, path = train_path)
 
-images, labels = load_mnist(dataset = "training", digit = [5], path = train_path)
-imshow(images.mean(axis=0), cmap = cm.gray)
-show()
+# let's see if it works
+# image number = whatever you want = 8 
+fig = pyplot.figure()
+ax = fig.add_subplot(1,1,1)
+image_plot = ax.imshow(images[10],cmap = mpl.cm.gray)
