22051d52283545efad1c5d8d47cea991766c0da1
Bill Eggert <weggert@princeton.edu>
2016-12-04 17:43:33 -0500

    moved code to dev folder

--- src/processing/dev/loader_base.py
+++ src/processing/dev/loader_base.py
@@ -8,13 +8,7 @@ Created on Thu Dec 01 15:42:10 2016
 
 class loader_base(object):
     
-    def __init__(self):
-        self.test_path "C:\Users\billi\Desktop\GitHub Repo\APC_FinalProject\data\mnist\testing"
-        self.train_path = "C:\Users\billi\Desktop\GitHub Repo\APC_FinalProject\data\mnist\training"
-        
-        self.train_images = []
-        self.test_images = []
-
+    @abc.abstractmethod
     def load_data(self):
         return
         
