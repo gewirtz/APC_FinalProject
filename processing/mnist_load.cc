321e1fe09fa1a403cbf83f59cd17db05839df2d5
Bill Eggert <weggert@princeton.edu>
2016-12-06 11:27:10 -0500

    got relative paths working for c++

---
+++ processing/mnist_load.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+// heavily inspired by "canonical" mnist data readers
+
+using namespace std;
+
+int main(){
+
+  //FILE* f;
+  //f = fopen("../data/mnist/test.txt","rb");
+
+  if (f == NULL) printf("FAIL!\n");
+  else printf("File opened\n");
+
+  string filename = "a";
+
+  //vector<vector<double>> vec;
+
+  //mnist_load(filename,vec);
+
+  cout << filename << "\n";
+
+  return 0;
+}
