e2747fba4b3ae49e90db0a3fbc9792264ec77508
Bill Eggert <weggert@princeton.edu>
2016-12-06 12:01:53 -0500

    progress on mnist c++ loader

--- processing/mnist_load.cc
+++ processing/mnist_load.cc
@@ -1,26 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
 
 // heavily inspired by "canonical" mnist data readers
 
 using namespace std;
 
+void load_mnist(string filename){
+
+
+  ifstream ifs (filename.c_str(), ifstream::in);
+  char c = ifs.get();
+
+  while(ifs.good()){
+    cout <<c;
+    c = ifs.get();
+  }
+
+  ifs.close();
+
+}
+
+
 int main(){
 
-  //FILE* f;
-  //f = fopen("../data/mnist/test.txt","rb");
+  string train_lbl = "../data/mnist/training/train-labels.idx1-ubyte";
+  string train_img = "../data/mnist/training/train-images.idx3-ubyte";
+  string test_lbl = "../data/mnist/testing/t10k-labels.idx1-ubyte";
+  string test_img = "../data/mnist/testing/t10k-images.idx3-ubyte";
 
-  if (f == NULL) printf("FAIL!\n");
-  else printf("File opened\n");
-
-  string filename = "a";
+  load_mnist(filename);
 
   //vector<vector<double>> vec;
 
   //mnist_load(filename,vec);
 
-  cout << filename << "\n";
+  //cout << filename << "\n";
 
   return 0;
 }
