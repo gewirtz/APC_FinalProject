988aed1f18355d3285d18f8a322782ae4a9b848d
NoemiVergopolan <noemi.v.rocha@gmail.com>
2016-12-07 23:49:05 -0500

    Performance

--- ModelFitting/Performance.cpp
+++ ModelFitting/Performance.cpp
@@ -3,9 +3,14 @@
 using namespace arma;
   
 
-  vec Performance::mse(double *label, double *predict_label){
+  vec Performance::mse2(double *label, double *predict_label){
 
   	return(mse);
+  }
+
+  Performance::mse(double *label, double *predict_label){
+    souble *mse=0.3
+    return(mse);
   }
   
 
--- ModelFitting/main_performance.cpp
+++ ModelFitting/main_performance.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char const *argv[]){
 	
-	// Training
+/*	// Training
 	double train = atof(argv[1]); //assumed we are getting this from somewhere and it is normalized
 	double train_labels = atof(argv[2]);
 	double *train2 = &train;
@@ -13,7 +13,7 @@ int main(int argc, char const *argv[]){
 	double test = atof(argv[1]); //assumed we are getting this from somewhere and it is normalized
 	double test_labels = atof(argv[2]);
 	double *test2 = &test;
-	double *test_labels2 = &test_labels;
+	double *test_labels2 = &test_labels;*/
 
 	// READ PARAMETERS OBJECT
 
@@ -30,12 +30,17 @@ int main(int argc, char const *argv[]){
 
 	// Print text file with stats
 
+	double *label1=[0,1,0,0,1,0,1,1,1,0];
+	double *label2=[0,1,1,1,1,1,1,1,1,1];
+
+	double *stat1=Performance::mse(label1,label2)
+
+	printf ("mse: %c \n",stat1);
 
 
-
-	GradientDescent *gd = new GradientDescent(100000, .001, .0001) ;
+/*	GradientDescent *gd = new GradientDescent(100000, .001, .0001) ;
 	LinearRegression *fit = new LinearRegression(train2, labels2, 0,0, gd);
 
-	LinearRegression *param= new LinearRegression::predict(fit)
+	LinearRegression *param= new LinearRegression::predict(fit)*/
 	return(0);
 }
