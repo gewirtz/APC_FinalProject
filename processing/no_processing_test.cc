05c104cc4acd8117f805525abe4bdc6f7c6e50fb
weggert2 <weggert@princeton.edu>
2016-12-12 17:43:25 -0500

    Create no_processing_test.cc

---
+++ processing/no_processing_test.cc
@@ -0,0 +1,12 @@
+
+#include "no_processing.h"
+
+No_processing* main(arma::mat* imported_data, arma::colvec* imported_labels){
+
+  No_processing p;
+  p = new No_processing();
+  p->set_Data(imported_data);
+  p->set_Labels(imported_labels);
+  p->process // process is in the NO_PROCESSING_H_ header definition
+  return(p*);
+ };
