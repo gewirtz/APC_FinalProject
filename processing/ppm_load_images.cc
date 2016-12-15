7c80ee343fc81308a231e7184234edd064925ae3
Andreas Rousing <arousing@princeton.edu>
2016-12-14 21:46:36 -0500

    PPM loader

---
+++ processing/ppm_load_images.cc
@@ -0,0 +1,124 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+using namespace std;
+using namespace arma;
+
+typedef struct {
+     unsigned char red,green,blue;
+} PPMPixel;
+
+typedef struct {
+     int x, y;
+     PPMPixel *data;
+} PPMImage;
+
+#define RGB_COMPONENT_COLOR 255
+
+static PPMImage *readPPM(const char *filename){
+
+         char buff[16];
+         PPMImage *img;
+         FILE *fp;
+         int c, rgb_comp_color;
+         //open PPM file for reading
+         fp = fopen(filename, "rb");
+         if (!fp) {
+              fprintf(stderr, "Unable to open file '%s'\n", filename);
+              exit(1);
+         }
+
+         //read image format
+         if (!fgets(buff, sizeof(buff), fp)) {
+              perror(filename);
+              exit(1);
+         }
+
+    //check the image format
+    if (buff[0] != 'P' || buff[1] != '6') {
+         fprintf(stderr, "Invalid image format (must be 'P6')\n");
+         exit(1);
+    }
+
+    //alloc memory form image
+    img = (PPMImage *)malloc(sizeof(PPMImage));
+    if (!img) {
+         fprintf(stderr, "Unable to allocate memory\n");
+         exit(1);
+    }
+
+    //check for comments
+    c = getc(fp);
+    while (c == '#') {
+    while (getc(fp) != '\n') ;
+         c = getc(fp);
+    }
+
+    ungetc(c, fp);
+    //read image size information
+    if (fscanf(fp, "%d %d", &img->x, &img->y) != 2) {
+         fprintf(stderr, "Invalid image size (error loading '%s')\n", filename);
+         exit(1);
+    }
+
+    //read rgb component
+    if (fscanf(fp, "%d", &rgb_comp_color) != 1) {
+         fprintf(stderr, "Invalid rgb component (error loading '%s')\n", filename);
+         exit(1);
+    }
+
+    //check rgb component depth
+    if (rgb_comp_color!= RGB_COMPONENT_COLOR) {
+         fprintf(stderr, "'%s' does not have 8-bits components\n", filename);
+         exit(1);
+    }
+
+    while (fgetc(fp) != '\n') ;
+    //memory allocation for pixel data
+    img->data = (PPMPixel*)malloc(img->x * img->y * sizeof(PPMPixel));
+
+    if (!img) {
+         fprintf(stderr, "Unable to allocate memory\n");
+         exit(1);
+    }
+
+    //read pixel data from file
+    if (fread(img->data, 3 * img->x, img->y, fp) != img->y) {
+         fprintf(stderr, "Error loading image '%s'\n", filename);
+         exit(1);
+    }
+
+    fclose(fp);
+    return img;
+}
+
+bool fileExists(const string& filename){
+    struct stat buf;
+    if (stat(filename.c_str(), &buf) != -1){
+        return true;
+    }
+    return false;
+}
+
+static PPMImage *ppm_load_images(const char *filename)
+    PPMImage *image;
+    int i=0;
+    string fileout = "cars_000";
+    while(fileExists(fileout)){
+        fileout = filename + "_" + to_string(i) + ".txt";
+        readPPM(fileout);
+        cout << fileout <<endl;
+        fileout = "cars_000"
+        i++;
+    }
+
+}
+
+
+int main(){
+    PPMImage *image;
+    image = ppm_load_images("can_bottom.ppm");
+    printf("Press any key...");
+    getchar();
+}
