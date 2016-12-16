2f70cdb3c69ee09f1d09a2e08c490dfd47c8512f
Andreas Rousing <arousing@princeton.edu>
2016-12-16 13:24:35 -0500

    PPM Proto

--- processing/ppm_load_images.cc
+++ processing/ppm_load_images.cc
@@ -1,5 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <armadillo>
+#include <sys/stat.h>
 
 
 using namespace std;
@@ -93,6 +95,20 @@ static PPMImage *readPPM(const char *filename){
     return img;
 }
 
+//Convert RGB pixels to greyscale
+
+//Convert sRGB to linear format
+double sRGB_to_linear(double x) {
+    if (x < 0.04045) return x/12.92;
+    return pow((x+0.055)/1.055, 2.4);
+}
+
+//sRGB gamma correction
+double linear_to_sRGB(double y) {
+    if (y <= 0.0031308) return 12.92 * y;
+    return 1.055 * pow(y, 1/2.4) - 0.055;
+}
+
 bool fileExists(const string& filename){
     struct stat buf;
     if (stat(filename.c_str(), &buf) != -1){
@@ -101,24 +117,63 @@ bool fileExists(const string& filename){
     return false;
 }
 
-static PPMImage *ppm_load_images(const char *filename)
-    PPMImage *image;
-    int i=0;
-    string fileout = "cars_000";
-    while(fileExists(fileout)){
-        fileout = filename + "_" + to_string(i) + ".txt";
-        readPPM(fileout);
-        cout << fileout <<endl;
-        fileout = "cars_000"
-        i++;
+void writePPM(const char *filename, PPMImage *img)
+{
+    FILE *fp;
+    //open file for output
+    fp = fopen(filename, "wb");
+    if (!fp) {
+         fprintf(stderr, "Unable to open file '%s'\n", filename);
+         exit(1);
     }
 
+    //write the header file
+    //image format
+    fprintf(fp, "P6\n");
+
+    //comments
+    //fprintf(fp, "# Created by %s\n",CREATOR);
+
+    //image size
+    fprintf(fp, "%d %d\n",img->x,img->y);
+
+    // rgb component depth
+    fprintf(fp, "%d\n",RGB_COMPONENT_COLOR);
+
+    // pixel data
+    fwrite(img->data, 3 * img->x, img->y, fp);
+    fclose(fp);
 }
+
+
+// vector<mat> ppm_load_images(string directory, string filename){
+//     PPMImage *image;
+//     int i=0;
+//     string fileout = "cars_000";
+//     while(fileExists(fileout)){
+//         fileout = filename + "_" + to_string(i);
+//         readPPM(fileout);
+//         cout << fileout <<endl;
+//         fileout = "cars_000"
+//         i++;
+//     }
+
+// }
 
 
 int main(){
     PPMImage *image;
-    image = ppm_load_images("can_bottom.ppm");
+    image = readPPM("car_0001.ppm");
+
+    // for()
+    // double R_linear = sRGB_to_linear(R/255.0);
+    // double G_linear = sRGB_to_linear(G/255.0);
+    // double B_linear = sRGB_to_linear(B/255.0);
+    // double gray_linear = 0.2126 * R_linear + 0.7152 * G_linear + 0.0722 * B_linear;
+
+    // double gray_color = round(linear_to_sRGB(gray_linear) * 255);
+    //image = ppm_load_images("can_bottom.ppm");
+    writePPM("test.ppm",image);
     printf("Press any key...");
     getchar();
 }  
