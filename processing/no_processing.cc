de9ba180989abf125789d75f31ecc560005e527e
ninagnedin <ninagnedin@gmail.com>
2017-01-06 14:09:40 -0600

    changed process function and split processed flag into train and test

--- processing/no_processing.cc
+++ processing/no_processing.cc
@@ -7,7 +7,7 @@ using namespace std;
 using namespace arma;
 
 //class No_processing : public No_processing{
-    
+
   //public:
 
 No_processing :: No_processing(){
@@ -16,7 +16,8 @@ No_processing :: No_processing(){
   //std::vector<arma::mat> data_test;
   //labels_train = new arma::colvec; //Check w Jeffrey
   //labels_test = new arma::colvec;
-  has_been_processed = 0;
+  train_has_been_processed = false;
+  test_has_been_processed = false;
 }
 
 No_processing :: ~No_processing(){
@@ -27,13 +28,27 @@ No_processing :: ~No_processing(){
   //delete labels_test;
 }
 
-void No_processing :: process(){
+
+//Process needs to be run twice
+//Train will always process first
+//This way user cant screw up referencing or feed in random data
+int No_processing :: process(){
   //Other versions of this class will have Gaussian stuff implemented here, etc.
   //This is the meat of the processing implementation work
-  if(has_been_processed == 0){
+  if(train_has_been_processed == false){
     //Don't let user process already processed data but also no need to throw up lots of errors
-    has_been_processed=1;
+    train_has_been_processed=true;
+    cout<<"Processing training data \n";
+    cout<<"Processing done \n";
+    return 0;
   }
+  if(test_has_been_processed == false){
+      test_has_been_processed=true;
+      cout<<"Processing testing data \n";
+      cout<<"Processing done \n";
+      return 0;
+  }
+  return 0;
 }
 
 // Pass the data and the labels
@@ -41,36 +56,36 @@ void No_processing :: set_data_train(std::vector<arma::mat > &d_train){
   //Same across all derived processing classes
    data_train = d_train;
 }
-  
+
 void No_processing :: set_data_test(std::vector<arma::mat > &d_test){
   //Same across all derived processing classes
   data_test=d_test;
 }
-  
+
 void No_processing :: set_labels_train(arma::colvec &l_train){
   //Same across all derived processing classes
   labels_train=l_train;
 }
-  
+
 void No_processing :: set_labels_test(arma::colvec &l_test){
   //Same across all derived processing classes
   labels_test=l_test;
 }
-  
-  
+
+
 // Get data and labels
 arma::colvec No_processing :: get_labels_train(){
   return(labels_train);
 }
-  
+
 arma::colvec No_processing :: get_labels_test(){
   return(labels_test);
 }
-    
+
 vector<arma::mat > No_processing :: get_data_train(){
   return(data_train);
 }
- 
+
 vector<arma::mat > No_processing ::get_data_test(){
   return(data_test);
 }
--- processing/no_processing.h
+++ processing/no_processing.h
@@ -10,26 +10,27 @@ class No_processing : public Data_processor{
   public:
     No_processing();
    ~No_processing();
-    
-    void process();
+
+    int process();
     void set_data_train(std::vector<arma::mat > &d_train);
     void set_data_test(std::vector<arma::mat > &d_test);
     void set_labels_train(arma::colvec &l_train);
     void set_labels_test(arma::colvec  &l_test);
-  
+
     arma::colvec get_labels_train();
     arma::colvec get_labels_test();
     std::vector<arma::mat > get_data_train();
     std::vector<arma::mat > get_data_test();
-      
+
   private:
-    int has_been_processed; //switch for whether data has been processed
+    bool train_has_been_processed; //switch for whether data has been processed
+    bool test_has_been_processed; //switch for whether data has been processed
     arma::colvec labels_train;
     arma::colvec labels_test;
     std::vector<arma::mat > data_train; //This will be the processed data for each class
     std::vector<arma::mat > data_test; //This will be the processed data for each class
 
-    
+
 };
 
 #endif //NO_PROCESSING_H_
