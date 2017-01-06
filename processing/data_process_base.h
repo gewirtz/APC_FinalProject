260d37120105f118f5de870aaf17f9e6f306f000
ninagnedin <ninagnedin@gmail.com>
2017-01-06 14:10:19 -0600

    changed return on process, split processed flag into train and test

--- processing/data_process_base.h
+++ processing/data_process_base.h
@@ -7,22 +7,23 @@
 
 class Data_processor {
  public:
-  //virtual ~Data_processor();
-  
-  virtual void process()=0;
+  virtual ~Data_processor();
+
+  virtual int process(); //Need to specify which data set to run on
   // consdier setting things to const &d_train, for example
   virtual void set_data_train(std::vector<arma::mat> &d_train)=0;
   virtual void set_data_test(std::vector<arma::mat> &d_test)=0;
   virtual void set_labels_train(arma::colvec &l_train)=0;
   virtual void set_labels_test(arma::colvec &l_test)=0;
- 
+
   virtual arma::colvec get_labels_train() = 0;
   virtual arma::colvec get_labels_test() = 0;
   virtual std::vector<arma::mat > get_data_train() = 0;
   virtual std::vector<arma::mat>  get_data_test() = 0;
 
  private:
-  int has_been_processed; //switch for whether data has been processed
+  bool train_has_been_processed; //switch for whether data has been processed
+  bool test_has_been_processed; //switch for whether data has been processed
   arma::colvec labels_train;
   arma::colvec labels_test;
   std::vector<arma::mat > data_train; //This will be the processed data for each class
