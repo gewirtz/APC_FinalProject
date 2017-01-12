e1e199105069984557ba32b5a63401b2eb5da578
NoemiVergopolan <noemi.v.rocha@gmail.com>
2017-01-11 20:27:03 -0500

    User

--- driver.py
+++ driver.py
@@ -23,7 +23,7 @@ t_unit = metadata["test_unit"]
 
 cmd = './alpha_driver "%s" "%s" "%s" "%s" "%s" "%s" %i %i %i' % (train_dir, test_dir, train_lbl, train_img, test_lbl, test_img, t_unit, preprocessing[0], model_fitting[0])
 print cmd
-os.system(cmd)
+#os.system(cmd)
 
 
 
